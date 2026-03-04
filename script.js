0db012f8efed13ca551378a9a5fcff5abc7cfa04
Camarederic <camarederic@gmail.com>
2026-03-04 23:31:46 +0300

    added media queries CSS

--- script.js
+++ script.js
@@ -2,7 +2,25 @@
 
 const container = document.querySelector(".container");
 const menuIcon = document.querySelector(".menu-icon");
+const headingRight = document.querySelector(".main-heading-right");
+const headingLeft = document.querySelector(".main-heading-left");
 
 menuIcon.addEventListener("click", () => {
   container.classList.toggle("navigate");
 });
+
+const responsiveDesign = () => {
+  if (window.innerWidth <= 700) {
+    headingRight.style.display = "none";
+    headingLeft.textContent = "explore";
+  } else {
+    headingRight.style.display = "block";
+    headingLeft.textContent = "exp";
+  }
+};
+
+window.addEventListener("resize", () => {
+  responsiveDesign();
+});
+
+responsiveDesign();
